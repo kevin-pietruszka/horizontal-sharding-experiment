6b08d6497d334c0228605e3335d3903f737cfa72
Jake Logan Michiels <jmichiels3@gatech.edu>
2023-04-02 19:33:43 -0400

    added shard communication

--- db_controller.py
+++ db_controller.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 from db_shard import db_shard
+import queue
 
 class db_controller:
     
+    output_queue = queue.Queue()
     def __init__(self) -> None:
         
         ids = np.array(range(1,101))
@@ -25,6 +27,23 @@ class db_controller:
             df["rating"] = data_split
             
             shards.append( db_shard("my_table_" +  str(int(i / split_size)), df) )
+
+    def execute_query(self, query:str, outputs:Queue) -> Queue:
+        for shard in shards:
+            shard.make_query(query, outputs)
+        
+        return outputs
+
+    def build_output(outputs:Queue) -> pandas.DataFrame:
+        df = []
+        for i in range(num_splits):
+            df.append(outputs.get())
+            outputs.task_done()
+        return pandas.DataFrame(df)
+
+
+
+
 
 if __name__ == "__main__":
     db_controller()
--- db_shard.py
+++ db_shard.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import duckdb
+import queue
+from threading import Thread
+from dp_controller import output_queue
 
 class db_shard:
     
@@ -7,9 +10,30 @@ class db_shard:
         
         self.id = id
         self.df = df
-        
+        self.q_queue = queue.Queue()
+        self.cond = True
+        self.listener = Thread(target = listen, args = ())
+
+
         create_table = f"CREATE TABLE {id} AS SELECT * FROM df;"
         duckdb.sql(create_table)
+
+
+
+    def listen(self):
+        while(True):
+            query, output = self.q_queue.get()
+            output.put(duckdb.sql(query).df())
+            self.q_queue.task_done()
+
+
+    def make_query(query:str, Queue:output) -> None:
+        self.q_queue.put((query, output))
+
+
+    def forward(self, node:db_shard):
+
+
         
     def __str__(self) -> str:
         return str(self.df)
