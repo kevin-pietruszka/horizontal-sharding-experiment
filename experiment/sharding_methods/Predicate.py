21c31d3747c0283b2cd894cc281fdcdf1a09d335
Kevin Pietruszka <kevinpiet13@gmail.com>
2023-04-25 16:17:27 -0400

    test predicat sharding

--- experiment/sharding_methods/Predicate.py
+++ experiment/sharding_methods/Predicate.py
@@ -8,11 +8,9 @@ def predicate_split(df: pd.DataFrame, column: str, predicates: List[sp.Interval]
     outputs = []
     num_splits = len(predicates)
     
-    
-    
     for i in range(num_splits):
         interval = predicates[i]
-        print(interval)
+        # print(interval)
         indices = []
         
         for index, val in enumerate(df[column]):
---
+++ experiment/test_predicate.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+import pandas as pd
+import sympy
+from sharding_methods.Predicate import predicate_split
+
+NUM_SERVERS = 5
+
+def main():
+    
+    ids = np.array(range(1,11))
+    ratings = np.random.rand(10)
+    ratings = np.multiply(ratings, 10)
+
+    df = pd.DataFrame()
+    df["id"] = ids
+    df["rating"] = ratings
+    
+    x = sympy.Symbol('x')
+    int1 = (0 <= x) & (x <= 2.5)
+    int2 = (2.5 <x) & (x <= 5)
+    int3 = (5 < x) & (x <= 7.5)
+    int4 = (7.5 < x) & (x <= 10)
+    
+    preds = [int1.as_set(), int2.as_set(), int3.as_set(), int4.as_set()]
+
+    frames = predicate_split(df,'rating', preds)
+    
+    for df in frames:
+        print(df)
+    
+if __name__ == '__main__':
+    main()
