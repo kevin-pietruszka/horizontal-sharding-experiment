4678fe3961f0dfa94a02133d4ca877524d63e7a8
kevin-pietruszka <kevinpiet13@gmail.com>
2023-04-29 19:42:29 -0400

    update data

--- experiment/collect_data.py
+++ experiment/collect_data.py
@@ -6,50 +6,64 @@ from my_statistics.StatisticsTable import StatisticsTable
 import random
 
 NUM_SERVERS = 5
-SIZE = 100
 NUM_QUERIES = 100
 COLUMN = 'rating'
 PRED_CHOICES = ['LT', 'LE', 'GT', 'GE']
 VALUE_RANGE = [0, 10]
+DATA_SIZE_SET = [100, 1000, 10000]
 
 def main():
     
-    ids = np.array(range(1,SIZE + 1))
-    ratings = np.random.rand(SIZE)
-    ratings = np.multiply(ratings, 10)
+    for data_size in DATA_SIZE_SET:
+        # Build Data
+        ids = np.array(range(1,data_size + 1))
+        ratings = np.random.rand(data_size)
+        ratings = np.multiply(ratings, 10)
+        df = pd.DataFrame()
+        df["id"] = ids
+        df["rating"] = ratings
 
-    df = pd.DataFrame()
-    df["id"] = ids
-    df["rating"] = ratings
+        # Can be any split
+        frames = even_split(df,NUM_SERVERS)
+        
+        # Build 
+        nodes = []
+        for i in range(NUM_SERVERS):
+            new_node = StarNode(i, frames[i])
+            nodes.append(new_node)
 
-    frames = even_split(df,NUM_SERVERS)
+        for i in range(len(nodes)):
+            others = nodes[:i] + nodes[i+1:]
+            nodes[i].set_connections(others)
+
+        queries = []
+
+        # Execute Queries
+        for i in range(NUM_QUERIES):
+            chosen_node = random.randint(0, NUM_SERVERS - 1)
+            pred = random.choice(PRED_CHOICES)
+            val = random.uniform(VALUE_RANGE[0], VALUE_RANGE[1])
+            queries.append((pred, val))
+            nodes[chosen_node].query(COLUMN, pred, val)
+        
+        # Collect the statistics
+        stats = []
+        for node in nodes:
+            stats.append(node.stats)
+        ouput_arr = StatisticsTable.combine(len(df), stats)
+
+        # Write to files
+        if data_size == 100:
+            print(ouput_arr)
+        
+        with open(f'./data/queries_{data_size}.txt', 'w') as qf:
+            for q in queries:
+                qf.write(str(q[0]) + ',' + str(q[1]))
+                qf.write('\n')
     
-    nodes = []
-    for i in range(NUM_SERVERS):
-        new_node = StarNode(i, frames[i])
-        nodes.append(new_node)
-
-    for i in range(len(nodes)):
-        others = nodes[:i] + nodes[i+1:]
-        nodes[i].set_connections(others)
-
-    chosen_node = random.randint(0, NUM_SERVERS - 1)
-
-    
-    for i in range(NUM_QUERIES):
-        pred = random.choice(PRED_CHOICES)
-        val = random.uniform(VALUE_RANGE[0], VALUE_RANGE[1])
-        query_output = nodes[chosen_node].query(COLUMN, pred, val)
-    
-    stats = []
-
-    for node in nodes:
-        stats.append(node.stats)
-
-    ouput_arr = StatisticsTable.combine(len(df), stats)
-    with open('./data/stats.npy', 'wb') as f:
-        df.to_csv("./data/stats_df.csv")
-        np.save(f, ouput_arr)
+        df.to_csv(f"./data/stats_df_{data_size}.csv")
+        with open(f'./data/stats_{data_size}.npy', 'wb') as f:
+            np.save(f, ouput_arr)
 
 
 if __name__ == "__main__":
