054f19f4c532fd8aecbb3df4e3433cfc62de6c87
kevin-pietruszka <kevinpiet13@gmail.com>
2023-04-29 16:58:29 -0400

    split predicates into ranges

--- experiment/util/predicates.py
+++ experiment/util/predicates.py
@@ -1,5 +1,25 @@
 from util.errors import InvalidPredicate
 import sympy
+from typing import List
+
+def predicate_interval(col_range: List[int], num_shards: int) -> List[sympy.Interval]:
+    _min = col_range[0]
+    _max = col_range[1]
+
+    step: float = float(_max - _min) / num_shards
+
+    output = list()
+    curr_val = _min
+    for i in range(num_shards):
+        if i == 0:
+            interval = sympy.Interval(curr_val, curr_val + step)
+        else:
+            interval = sympy.Interval(curr_val, curr_val + step, left_open=True)
+
+        output.append(interval)
+        curr_val += step
+
+    return output
 
 def query_in_interval(predicate: str, value: int, interval: sympy.Interval) -> bool:
     
